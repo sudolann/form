4b3f72eba1c75389aa0096033c2e6f0ec39b0c15
Admin <admin@MacBook-Pro-Admin.local>
2020-09-07 18:28:11 +0200

    eventList mapped data

--- frontend/src/components/eventsList/EventsList.tsx
+++ frontend/src/components/eventsList/EventsList.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, ReactElement, useEffect, useState } from 'react';
+import React, {
+  FunctionComponent,
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 import { ErrorAlert } from '../errorAlert/ErrorAlert';
 import { EventProps } from '../eventById/EventById';
 import { LoadingBar } from '../loadingBar/LoadingBar';
@@ -7,7 +12,7 @@ import './EventsList.scss';
 import { useHttpClient } from '../../hooks/useHttpClient';
 
 export interface EventPropsState extends EventProps {
-  id: string
+  id: string;
 }
 
 export const EventsList: FunctionComponent = (): ReactElement => {
@@ -16,26 +21,49 @@ export const EventsList: FunctionComponent = (): ReactElement => {
   useEffect((): void => {
     const fetchEvent = async (): Promise<void> => {
       try {
-        const responseData = await sendRequest(`http://localhost:${process.env.REACT_APP_BACKEND_PORT}/events`);
-        setEventsList(responseData);
-      } catch (err) {
-      }
+        const responseData = await sendRequest(
+          `http://localhost:${process.env.REACT_APP_BACKEND_PORT}/events`,
+        );
+
+        const dataMapped = responseData.map(
+          (data: { _id: any; name: any; email: any; date: any }) => {
+            const { _id, name, email, date } = data;
+            return { id: _id, name, email, date };
+          },
+        );
+        setEventsList(dataMapped);
+      } catch (err) {}
     };
     fetchEvent();
   }, [sendRequest]);
 
-  if(isLoading) {
-    return <LoadingBar/>
+  if (isLoading) {
+    return <LoadingBar />;
   }
-  if(error) {
-    return <ErrorAlert errorMessage="error with fetch events" fullPage/>
+  if (error) {
+    return (
+      <ErrorAlert errorMessage="error with fetch events" fullPage />
+    );
   }
   return (
     <ul className="list">
-    {eventsList && (eventsList.length === 0 ? <ErrorAlert errorMessage="No events" fullPage/> : eventsList.map((event: EventPropsState)=> {
-      const {name, date, email, id } = event;
-      return <EventListItem name={name} date={date} email={email} key={id} id={id} />
-    }))}
+      {eventsList &&
+        (eventsList.length === 0 ? (
+          <ErrorAlert errorMessage="No events" fullPage />
+        ) : (
+          eventsList.map((event: EventPropsState) => {
+            const { name, date, email, id } = event;
+            return (
+              <EventListItem
+                name={name}
+                date={date}
+                email={email}
+                key={id}
+                id={id}
+              />
+            );
+          })
+        ))}
     </ul>
   );
 };
