e1454454fb823c918e4aca17247398df19ccb2f0
Admin <admin@MacBook-Pro-Admin.local>
2020-09-07 13:40:02 +0200

    updated hook

--- frontend/src/hooks/useHttpClient.ts
+++ frontend/src/hooks/useHttpClient.ts
@@ -1,32 +1,22 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<null |string>(null);
-
-  const activeHttpRequests: any = useRef([]);
   
   const sendRequest = useCallback(
     async (url: RequestInfo, method = 'GET', body = null, headers = {}) => {
       setIsLoading(true);
-      const httpAbortCtrl: any = new AbortController()
-      activeHttpRequests.current.push(httpAbortCtrl);
 
       try {
         const response = await fetch(url, {
           method,
           body,
           headers,
-          signal: httpAbortCtrl.signal
         });
 
         const responseData = await response.json();
 
-        activeHttpRequests.current = activeHttpRequests.current.filter(
-          (reqCtrl: any) => {
-            return reqCtrl !== httpAbortCtrl;
-          }
-        );
 
         if (!response.ok) {
           throw new Error(responseData.message);
@@ -42,14 +32,6 @@ export const useHttpClient = () => {
     },
     []
   );
-
-
-  useEffect(() => {
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      activeHttpRequests.current.forEach((abortCtrl: { abort: () => any; }) => abortCtrl.abort());
-    };
-  }, []);
 
   return { isLoading, error, sendRequest, setError };
 };
